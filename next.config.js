447207ea842f81eab54b515449226a9a4995f7ce
Ramazan Azimli <80627823+antisyst@users.noreply.github.com>
2023-07-28 14:54:43 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,40 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+const webpack = require('webpack');
 const nextConfig = {
-  reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
 
-module.exports = nextConfig
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next',
+            name: 'static/media/[name].[hash].[ext]',
+          },
+        },
+      ],
+    });
+
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        default: false,
+        vendors: false,
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      };
+    }
+
+    return config;
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
